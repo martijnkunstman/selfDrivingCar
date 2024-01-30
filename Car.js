fd19c6482f740a7df4031a22f197fe879c63fed6
Martijn Kunstman <martijn@kunstman.nl>
2024-01-30 10:19:46 +0100

    car

---
+++ Car.js
@@ -0,0 +1,27 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.angle = 0;
+
+    this.left = false;
+    this.right = false;
+    this.up = false;
+    this.down = false;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
+}
--- main.js
+++ main.js
@@ -9,3 +9,13 @@ genetic algorithms
 
 */
 
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+
+
