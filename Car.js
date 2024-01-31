b41c3dec183112e99678d878c2d7c3bd9d066ac8
Martijn Kunstman <martijn@kunstman.nl>
2024-01-31 09:37:53 +0100

    steering

--- Car.js
+++ Car.js
@@ -9,42 +9,60 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
+    this.angle = 0;
 
     this.controls = new Controls();
   }
 
-  update() {    
+  update() {
+    this.#move();
+  }
+
+  #move() {
     if (this.controls.up) {
-      this.speed+=this.acceleration
+      this.speed += this.acceleration;
     }
     if (this.controls.down) {
-        this.speed-=this.acceleration
-    }   
+      this.speed -= this.acceleration;
+    }
     if (this.speed > this.maxSpeed) {
-        this.speed = this.maxSpeed;
+      this.speed = this.maxSpeed;
     }
-    if (this.speed < -this.maxSpeed/2) {
-        this.speed = -this.maxSpeed/2;
-    }
-
-    if (this.speed>0){
-        this.speed-=this.friction;
-    }
-    if (this.speed<0){
-        this.speed+=this.friction;
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
     }
 
-    this.y -=this.speed;
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    this.y = this.y - this.speed * Math.cos(this.angle);
+    this.x = this.x - this.speed * Math.sin(this.angle);
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+    ctx.restore;
   }
 }
--- main.js
+++ main.js
@@ -16,9 +16,7 @@ const car = new Car(100,100,30,50);
 animate();
 
 function animate() {
-  car.update();
   canvas.height = window.innerHeight;
-  car.draw(ctx);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   car.update();
   car.draw(ctx);
