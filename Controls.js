5c60ce95a0c5a7d6087a5731ed70463d72453d37
Martijn Kunstman <martijn@kunstman.nl>
2024-04-10 19:40:48 +0200

    training car

--- Controls.js
+++ Controls.js
@@ -1,52 +1,52 @@
-class Controls {
-  constructor(type) {
-    this.left = false;
-    this.right = false;
-    this.up = false;
-    this.down = false;
-    switch (type) {
-      case "KEYS":
-        this.#addKeyboardListeners();
-        break;
-      case "DUMMY":
-        this.up = true;
-        break;
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
-  }
-  #addKeyboardListeners() {
-    document.onkeydown = (e) => {
-      switch (e.key) {
-        case "ArrowLeft":
-          this.left = true;
-          break;
-        case "ArrowRight":
-          this.right = true;
-          break;
-        case "ArrowUp":
-          this.up = true;
-          break;
-        case "ArrowDown":
-          this.down = true;
-          break;
-      }
-      //console.table(this);
-    };
-    document.onkeyup = (e) => {
-      switch (e.key) {
-        case "ArrowLeft":
-          this.left = false;
-          break;
-        case "ArrowRight":
-          this.right = false;
-          break;
-        case "ArrowUp":
-          this.up = false;
-          break;
-        case "ArrowDown":
-          this.down = false;
-          break;
-      }
-      //console.table(this);
-    };
-  }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+        }
+    }
 }
